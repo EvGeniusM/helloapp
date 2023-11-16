5dee5d0a4a09e7a3bcdb079bbfe8eef498250c36
black <black@example.com>
2023-11-16 10:38:55 +0500

    Greeted black and white

--- hello_script/hello_script/Program.cs
+++ hello_script/hello_script/Program.cs
@@ -7,6 +7,8 @@ namespace hello_script
         public static void Main()
         {
             Console.WriteLine("hello everyone");
+            GreetWhite();
+            GreetBlack();
         }
 
         public static void GreetWhite()
