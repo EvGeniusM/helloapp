fe3bcd60245befda1469217e03b674ce3c12f457
black <black@example.com>
2023-11-16 10:05:23 +0500

    Initial commit

---
+++ hello_script/hello_script/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_script
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
